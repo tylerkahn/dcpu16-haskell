70d70868b3541fb2b510bcc1bb9e7821114bcde8
Tyler Kahn <tkahn6@gmail.com>
2012-04-06 18:23:33 -0400

    more aesthetics improvements

--- DCPU16/Emulator.hs
+++ DCPU16/Emulator.hs
@@ -16,20 +16,20 @@ replaceNth n newVal (x:xs)
 access :: Integral a => a -> [b] -> b
 access = flip (!!) . fromIntegral
 
-data DCPUState = DCPUState { ram :: [Word16], registerFile :: [Word16],
+data DCPUState = DCPUState { mem :: [Word16], registerFile :: [Word16],
     sp :: Word16, pc :: Word16, o :: Word16, cycles :: Int, hlt :: Word16}
         deriving (Eq)
 
 instance Show DCPUState where
-    show (DCPUState ram regFile sp pc o cycles hlt) =
+    show (DCPUState mem regFile sp pc o cycles hlt) =
         concatMap (\(l, v) -> l ++ ": " ++ show v ++ " ") pairs  ++ "\n" where
-            pairs = [("HLT", hlt), ("PC", pc), ("SP", sp), ("*SP", access sp ram), ("O", o)] ++
+            pairs = [("HLT", hlt), ("PC", pc), ("SP", sp), ("*SP", access sp mem), ("O", o)] ++
                 zip ["A","B","C","X","Y","Z","I","J"] regFile
 
 data Instruction = Instruction BasicOpcode Word8 Word8
     deriving (Eq, Show)
 
-data CellAddr = RAM Word16 | Reg Word8 | SP | PC | O | Literal
+data CellAddr = Mem Word16 | Register Word8 | SP | PC | O | Literal
     deriving (Eq, Show)
 
 data BasicOpcode = NonBasic | SET | ADD | SUB | MUL | DIV | MOD
@@ -42,8 +42,8 @@ data NonBasicOpcode = Reserved | JSR | HLT
 startingDCPUState = DCPUState (replicate 0x10000 0) (replicate 8 0) 0xffff 0 0 0 0
 
 loadProgram :: [Word16] -> DCPUState -> DCPUState
-loadProgram p cpu = cpu { ram = ram' } where
-    ram' = p ++ replicate (0x10000 - length p) 0
+loadProgram p cpu = cpu { mem = mem' } where
+    mem' = p ++ replicate (0x10000 - length p) 0
 
 decodeWord :: Integral a => Word16 -> (BasicOpcode, a, a)
 decodeWord x = (toEnum $ fromIntegral o, fromIntegral a, fromIntegral b) where
@@ -52,80 +52,68 @@ decodeWord x = (toEnum $ fromIntegral o, fromIntegral a, fromIntegral b) where
     b = (x `shiftR` 10) .&. 0x3f
 
 currentWord :: DCPUState -> Word16
-currentWord cpu = (!!) (ram cpu) $ fromIntegral (pc cpu)
+currentWord cpu = (!!) (mem cpu) $ fromIntegral (pc cpu)
 
 nextWord :: State DCPUState Word16
-nextWord = do
-        modify (\cpu -> cpu { pc = 1 + pc cpu })
-        gets currentWord
+nextWord = modify (\cpu -> cpu { pc = 1 + pc cpu }) >> gets currentWord
 skip = void nextWord
 
 push :: Word16 -> State DCPUState ()
 push x = do
-    sp' <- liftM (+ (-1)) (gets sp)
-    modify (\cpu -> cpu {sp = sp', ram = replaceNth sp' x (ram cpu) })
+    sp' <- liftM (+ (-1)) $ gets sp
+    modify (\cpu -> cpu {sp = sp', mem = replaceNth sp' x (mem cpu) })
 
 incCyclesBy :: Int -> State DCPUState ()
-incCyclesBy n = do
-        cpu <- get
-        put $ cpu { cycles = n + cycles cpu }
+incCyclesBy n = modify (\cpu -> cpu { cycles = n + cycles cpu })
 
 operandAction :: Integral a => a -> State DCPUState (Word16, CellAddr)
 operandAction a
     | a <= 0x7 = do
-            val <- (access a . registerFile) `fmap` get
-            return (val, Reg $ fromIntegral a)
+            val <- gets $ access a . registerFile
+            return (val, Register $ fromIntegral a)
     | a <= 0x0f = do
-            rVal <- (access (a - 0x8) . registerFile) `fmap` get
-            val <- (access rVal . ram) `fmap` get
-            return (val, RAM rVal)
+            rVal <- gets $ access (a - 0x8) . registerFile
+            val <- gets $ access rVal . mem
+            return (val, Mem rVal)
     | a <= 0x17 = do
-            rVal <- (access (a - 0x10) . registerFile) `fmap` get
+            rVal <- gets $ access (a - 0x10) . registerFile
             nWord <- nextWord
             let addr = rVal + nWord
-            val <- (access addr . ram) `fmap` get
-            return (val, RAM addr)
+            val <- gets $ access addr . mem
+            return (val, Mem addr)
     | a == 0x18 = do
             cpu <- get
             put $ cpu { sp = 1 + sp cpu }
-            let val = access (sp cpu) (ram cpu)
-            return (val, RAM $ sp cpu)
+            let val = access (sp cpu) (mem cpu)
+            return (val, Mem $ sp cpu)
     | a == 0x19 = do
             cpu <- get
-            let val = access (sp cpu) (ram cpu)
-            return (val, RAM (sp cpu))
+            let val = access (sp cpu) (mem cpu)
+            return (val, Mem (sp cpu))
     | a == 0x1a = do
-            cpu <- get
-            let sp' = sp cpu - 1
-            put $ cpu { sp = sp' }
-            val <- (access sp' . ram) `fmap` get
-            return (val, RAM sp')
+            modify (\cpu -> cpu { sp = sp cpu - 1})
+            sp' <- gets sp
+            val <- gets $ access sp' . mem
+            return (val, Mem sp')
     | a == 0x1b = liftM ((, SP) . sp) get
     | a == 0x1c = liftM ((, PC) . pc) get
     | a == 0x1d = liftM ((, O) . o) get
     | a == 0x1e = do
             nWord <- nextWord
-            val <- (access nWord . ram) `fmap` get
-            return (val, RAM nWord)
+            val <- gets $ access nWord . mem
+            return (val, Mem nWord)
     | a == 0x1f = liftM (, Literal) nextWord
     | otherwise = return (fromIntegral (a - 32), Literal)
 
 skipNextInstruction = do
     cpu <- get
-    (_, arg1, arg2) <- fmap decodeWord nextWord
+    (_, arg1, arg2) <- decodeWord `fmap` nextWord
     operandAction arg1 >> operandAction arg2
-    cpu' <- get
-    put $ cpu { pc = (pc cpu') }
+    modify (\cpu' -> cpu { pc = pc cpu' })
 
 set :: CellAddr -> Word16 -> State DCPUState ()
-set (Reg a) b = do
-            cpu <- get
-            let regFile' = replaceNth a b (registerFile cpu)
-            put $ cpu {registerFile = regFile'}
-set (RAM a) b = do
-            cpu <- get
-            let ram' = replaceNth a b (ram cpu)
-            put $ cpu {ram = ram'}
+set (Register a) b = modify (\cpu -> cpu { registerFile = replaceNth a b (registerFile cpu) })
+set (Mem a) b = modify (\cpu -> cpu { mem = replaceNth a b (mem cpu) })
 set SP b = modify (\cpu -> cpu { sp = b })
 set PC b = modify (\cpu -> cpu { pc = b - 1 })
 set O b = modify (\cpu -> cpu { o = b })
@@ -135,22 +123,18 @@ instructionAction' :: BasicOpcode -> Word32 -> Word32 -> CellAddr -> State DCPUS
 instructionAction' op a b addr
     | op == SET = set addr (fromIntegral b)
     | op == ADD = do
-        let sum = a + b
-        set O (if testBit sum 16 then 0x1 else 0x0)
-        set addr $ fromIntegral sum
+        set O (if testBit (a+b) 16 then 0x1 else 0x0)
+        set addr $ fromIntegral (a+b)
     | op == SUB = do
-        let diff = a - b
-        set O (if testBit diff 16 then 0xffff else 0x0)
-        set addr $ fromIntegral diff
+        set O (if testBit (a-b) 16 then 0xffff else 0x0)
+        set addr $ fromIntegral (a-b)
     | op == MUL = do
-        let prod = a * b
-        set O $ fromIntegral (prod `shiftR` 16)
-        set addr $ fromIntegral prod
+        set O $ fromIntegral ((a*b) `shiftR` 16)
+        set addr $ fromIntegral (a*b)
     | op == DIV = case b of 0 -> set addr 0 >> set O 0
                             _ -> do
-                                let quotient = div a b
                                 set O $ fromIntegral ((a `shiftL` 16) `div` b)
-                                set addr $ fromIntegral quotient
+                                set addr $ fromIntegral $ div a b
     | op == MOD = case b of 0 -> set addr 0
                             _ -> set addr $ fromIntegral $ mod a b
     | op == SHL = do
@@ -164,10 +148,10 @@ instructionAction' op a b addr
     | op == AND = set addr $ fromIntegral $ a .&. b
     | op == BOR = set addr $ fromIntegral $ a .|. b
     | op == XOR = set addr $ fromIntegral $ a `xor` b
-    | op == IFE = unless (a == b) $ skipNextInstruction
-    | op == IFN = unless (a /= b) $ skipNextInstruction
-    | op == IFG = unless (a > b) $ skipNextInstruction
-    | op == IFB = unless ((a .&. b) /= 0) $ skipNextInstruction
+    | op == IFE = unless (a == b) skipNextInstruction
+    | op == IFN = unless (a /= b) skipNextInstruction
+    | op == IFG = unless (a > b) skipNextInstruction
+    | op == IFB = unless ((a .&. b) /= 0) skipNextInstruction
 
 instructionAction :: Integral a => BasicOpcode -> a -> a -> State DCPUState ()
 instructionAction NonBasic o a = do
@@ -188,7 +172,7 @@ nonBasicInstructionAction op a addr
 
 pulse :: State DCPUState DCPUState
 pulse = do
-    (op, arg1, arg2) <- fmap decodeWord $ gets currentWord
+    (op, arg1, arg2) <- gets $ decodeWord . currentWord
     instructionAction op arg1 arg2
     skip >> get
 
